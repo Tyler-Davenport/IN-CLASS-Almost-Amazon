6baa66b30cda36948abfb9a8ee7cc9eace1880ad
Tyler-Davenport <tydavenport0415@gmail.com>
2024-11-12 19:19:32 -0600

    added the add function and edit function

--- events/domEvents.js
+++ events/domEvents.js
@@ -1,6 +1,7 @@
 /* eslint-disable quotes */
-import { deleteBook, getBooks } from "../api/bookData";
+import { deleteBook, getBooks, getSingleBook } from "../api/bookData";
 import { showBooks } from "../pages/books";
+import addBookForm from "../components/forms/addBookForm";
 
 const domEvents = () => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
@@ -19,13 +20,15 @@ const domEvents = () => {
 
     // TODO: CLICK EVENT FOR SHOWING FORM FOR ADDING A BOOK
     if (e.target.id.includes('add-book-btn')) {
-      console.warn('ADD BOOK');
+      addBookForm();
     }
 
     // TODO: CLICK EVENT EDITING/UPDATING A BOOK
     if (e.target.id.includes('edit-book-btn')) {
-      console.warn('EDIT BOOK', e.target.id);
-      console.warn(e.target.id.split('--'));
+      const [, firebaseKey] = e.target.id.split('--');
+
+      getSingleBook(firebaseKey).then((bookObj) => addBookForm(bookObj));
+      // getSingleBook(firebaseKey).then(addBookForm); // using the callback method
     }
     // TODO: CLICK EVENT FOR VIEW BOOK DETAILS
     if (e.target.id.includes('view-book-btn')) {
