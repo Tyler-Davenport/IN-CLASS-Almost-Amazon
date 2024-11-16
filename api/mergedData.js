94d2c98cb6327d0bcb6f256000a6ce934800d280
Tyler-Davenport <tydavenport0415@gmail.com>
2024-11-16 12:54:34 -0600

    author details is pulling up but book values undefined

--- api/mergedData.js
+++ api/mergedData.js
@@ -1,1 +1,32 @@
+import { getSingleAuthor } from './authorData';
+import { getBooks, getSingleBook } from './bookData';
+
 // for merged promises
+// TODO: Get data for viewBook
+const getBookDetails = async (firebaseKey) => {
+  const bookObject = await getSingleBook(firebaseKey);
+  const authorObject = await getSingleAuthor(bookObject.author_id);
+  return { ...bookObject, authorObject };
+};
+
+// const getBookDetails = (firebaseKey) => new Promise((resolve, reject) => {
+//   // GET SINGLE BOOK
+//   getSingleBook(firebaseKey).then((bookObject) => { // returns single book object
+//     getSingleAuthor(bookObject.author_id) // we nest this promise so that we can use the book object
+//       .then((authorObject) => resolve({ ...bookObject, authorObject }));
+//   }).catch(reject);
+//   // GET AUTHOR
+//   // Create an object that has book data and an object named authorObject
+// });
+
+// get data for viewAuthor
+const getAuthorDetails = async (firebaseKey) => {
+  const authorObject = await getSingleAuthor(firebaseKey);
+  const bookObject = await getBooks(authorObject.book_id);
+  return { ...authorObject, bookObject };
+};
+
+export {
+  getAuthorDetails,
+  getBookDetails
+};
